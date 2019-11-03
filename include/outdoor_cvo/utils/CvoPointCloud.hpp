4b74f1864cdf3486eb4a934f697a5f0feac66063
zeroAska <trotwoodray@gmail.com>
2019-11-03 14:16:16 -0500

    add code for reading pointcloud files

--- include/outdoor_cvo/utils/CvoPointCloud.hpp
+++ include/outdoor_cvo/utils/CvoPointCloud.hpp
@@ -1,26 +1,38 @@
 #pragma once
+#include <string>
 #include "utils/data_type.hpp"
 namespace cvo {
 
 
-  struct CvoPointCloud{
-    //public:
+  class CvoPointCloud{
+  public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
     
     CvoPointCloud(const cv::Mat & left_image,
                   const cv::Mat & right_image);
+    CvoPointCloud();
     CvoPointCloud(const cv::Mat & left_image,
                   const cv::Mat & right_image,
                   int num_semantic_class,
                   const cv::Mat & semantic_left_img);
     ~CvoPointCloud();
+
+    int read_cvo_pointcloud_from_file(const std::string & filename);
+
+    // getters
+    int num_points() const {return num_points_;}
+    int num_classes() const {return num_classes_;}
+    const ArrayVec3f & positions() const {return positions_;}
+    const Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> & labels() const { return labels_;}
+    const Eigen::Matrix<float, Eigen::Dynamic, 5> & features() const {return features_;}
     
-    int num_points;
-    int num_classes;
+  private:
+    int num_points_;
+    int num_classes_;
     
-    ArrayVec3f positions;  // points position. x,y,z
-    Eigen::Matrix<float, Eigen::Dynamic, 5> features;   // rgb, gradient
-    Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> labels; // number of points by number of classes  
+    ArrayVec3f positions_;  // points position. x,y,z
+    Eigen::Matrix<float, Eigen::Dynamic, 5> features_;   // rgb, gradient in [0,1]
+    Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> labels_; // number of points by number of classes  
     // 0. building 1. sky 2. road
     // 3. vegetation 4. sidewalk 5. car 6. pedestrian
     // 7. cyclist 8. signate 9. fence 10. pole
---
+++ src/main_cvo_align.cpp
@@ -0,0 +1,95 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <cmath>
+#include <boost/filesystem.hpp>
+#include "utils/CvoPointCloud.hpp"
+using namespace std;
+using namespace boost::filesystem;
+
+
+
+int main(int argc, char *argv[]) {
+  // list all files in current directory.
+  //You could put any file path in here, e.g. "/home/me/mwah" to list that directory
+  int num_class= 0;
+  string n_class_str;
+  if (argc > 5) {
+    n_class_str = argv[5];
+    num_class = stoi(n_class_str);
+  }
+  
+  
+  path p (argv[1] );
+  std::ofstream output_file(argv[2]);
+  int start_frame = stoi(argv[3]);
+  int num_frames = stoi(argv[4]);
+  
+  
+  vector<string> files;
+  // cycle through the directory
+  int total_num = 0;
+  for(auto & p : boost::filesystem::directory_iterator( p ) )
+  {
+    // If it's not a directory, list it. If you want to list directories too, just remove this check.
+    if (is_regular_file(p.path())) {
+      // assign current file name to current_file and echo it out to the console.
+      string current_file = p.path().string();
+      files.push_back(string(argv[1]) + "/" + to_string(total_num) + ".txt" );
+      total_num += 1;
+      //cout <<"reading "<< current_file << endl; 
+
+    }
+  }
+
+  std::cout<<"Just read file names\n";
+  //sort(files.begin(), files.end());
+  //vector<vector<dso::CvoTrackingPoints> > all_pts(files.size());
+  //vector<vector<dso::CvoTrackingPoints>> downsampled(files.size());
+  std::vector<cvo::CvoPointCloud> pc_vec(files.size());
+  int i = 0;
+  for (auto &f:  files) {
+    std::cout<<"Reading "<<f<<std::endl;
+    //dso::read_cvo_pointcloud_from_file<dso::CvoTrackingPoints>(f, all_pts[i]);
+    pc_vec[i].read_cvo_pointcloud_from_file(f);
+    i ++;
+    if (i == num_frames) break;
+  }
+  std::cout<<"Just reading  names\n";
+
+  /*
+  cvo::rkhs_se3 cvo_align;
+  Eigen::Affine3f init_guess;
+  init_guess.matrix().setIdentity();
+  init_guess.matrix()(2, 3) = -0.75;
+
+  for (int i = start_frame; i< downsampled.size()-2 ; i++) {
+    if (i > start_frame)
+      init_guess = cvo_align.get_transform().inverse();
+    //    init_guess.matrix()(2,3) = -2.5;
+    //init_guess.setIdentity();
+    std::cout<<"\n=============================================\nat"<<i<<"\n iter";
+    cvo_align.set_pcd(downsampled[0+i], downsampled[1+i], init_guess, true);
+    cvo_align.align();
+    init_guess= cvo_align.get_accum_transform();
+    Eigen::Matrix4f result = init_guess.matrix();
+    std::cout<<"\n The inner product between "<<i <<" and "<< i+1 <<" is "<<cvo_align.inner_product()<<"\n";
+    std::cout<<"Transform is \n";
+    std::cout<<cvo_align.get_transform().matrix() <<"\n\n";
+     output_file << result(0,0)<<" "<<result(0,1)<<" "<<result(0,2)<<" "<<result(0,3)<<" "
+                <<result(1,0)<<" " <<result(1,1)<<" "<<result(1,2)<<" "<<result(1,3)<<" "
+                <<result(2,0)<<" " <<result(2,1)<<" "<<result(2,2)<<" "<<result(2,3);
+     //output_file << result.block<3,4>(0,0);
+    output_file<<"\n";
+    output_file<<std::flush;
+
+  }
+  
+
+  output_file.close();
+  */
+  return 0;
+}
