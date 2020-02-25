540c1d0ea7d2a5ef7f6c7e43a43584aa401264fa
zeroAska <trotwoodray@gmail.com>
2020-02-25 02:35:05 -0500

    local tests

--- scripts/single_param_test.bash
+++ scripts/single_param_test.bash
@@ -1,14 +1,16 @@
 cd build && make -j && cd ..
 
-./build/bin/acvo_align_semantic_gpu_raw_img /home/rayzhang/data/kitti/04 cvo_params/acvo_params_gpu.txt \
+./build/bin/acvo_align_semantic_gpu_raw_img /home/rzh/media/sda1/ray/datasets/kitti/sequences/04 cvo_params/acvo_params_gpu.txt \
                                        accum04.txt 0 800 
 
 
+./build/bin/acvo_align_semantic_gpu_raw_img /home/rzh/media/sda1/ray/datasets/kitti/sequences/04 cvo_params/acvo_params_gpu_2.4.txt \
+                                       accum04_2.4.txt 0 800 
 
-./build/bin/acvo_align_gpu_semantic_raw_img /home/rayzhang/data/kitti/04 cvo_params/acvo_params_gpu_1.6.txt \
+./build/bin/acvo_align_gpu_semantic_raw_img /home/rzh/media/sda1/ray/datasets/kitti/sequences/04 cvo_params/acvo_params_gpu_1.6.txt \
                                        accum04_1.6.txt 0 800
 
 
 
-./build/bin/acvo_align_gpu_semantic_raw_img /home/rayzhang/data/kitti/04 cvo_params/acvo_params_gpu_3.2.txt \
+./build/bin/acvo_align_gpu_semantic_raw_img /home/rzh/media/sda1/ray/datasets/kitti/sequences/04 cvo_params/acvo_params_gpu_3.2.txt \
                                        accum04_3.2.txt 0 800
