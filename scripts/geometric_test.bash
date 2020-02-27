e140003293f8219e633d86ed5ed0df5ab8b18808
zeroAska <trotwoodray@gmail.com>
2020-02-26 22:50:15 -0500

    tune params for lidar

--- scripts/geometric_test.bash
+++ scripts/geometric_test.bash
@@ -1,5 +1,5 @@
 
 cd build && make -j && cd .. && \
 
-./build/bin/acvo_geometric_gpu_raw_lidar /home/rayzhang/data/03 cvo_params/acvo_geometric_params_gpu.txt \
-                                       acvo_geometric_03.txt 0 1000
+    ./build/bin/acvo_geometric_gpu_raw_lidar /home/rayzhang/data/kitti_lidar/dataset/sequences/00 cvo_params/acvo_geometric_params_gpu.txt \
+                                       acvo_geometric_00.txt 3600   10000
