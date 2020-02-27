8643e4510db4d5e808253ca70cd045a33c2020bf
zeroAska <trotwoodray@gmail.com>
2020-02-27 15:12:12 -0500

    local changes for sunny

--- scripts/multiple_test.bash
+++ scripts/multiple_test.bash
@@ -1,8 +1,8 @@
 
 
 cd build && make -j && cd ..
-
-for i in 00 01 02 03 04 05 06 07 08 09 10
+export CUDA_VISIBLE_DEVICES=1
+for i in 02 03 04 05 
 do
     echo "running $i..." && \
         ./build/bin/acvo_align_semantic_gpu_raw_img /home/rzh/media/sda1/ray/datasets/kitti/sequences/${i} cvo_params/acvo_params_gpu.txt accum${i}_semantic.txt 0 10000 && \
