456b4a4ed98604a077c8293ff8094363a3150c42
zeroAska <trotwoodray@gmail.com>
2020-04-14 03:58:57 +0000

    merge

--- scripts/cvo_intensity_img_test.bash
+++ scripts/cvo_intensity_img_test.bash
@@ -1,7 +1,7 @@
 
 cd build && make -j && cd .. && \
-for i in 04
-	#for i in 00 01 02 03 04 05 06 07 08 09 10
+#for i in 04
+for i in 05 06 07 
 do
     ./build/bin/cvo_align_gpu_raw_img /home/rzh/media/sda1/ray/datasets/kitti/sequences/$i cvo_params/cvo_intensity_params_img.yaml \
                                        cvo_img_intensity_$i.txt 0 10000
