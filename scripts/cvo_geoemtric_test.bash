c8a60ffabd51cab39872da4d7b1eb0ac842cdcb4
zeroAska <trotwoodray@gmail.com>
2020-05-14 15:41:23 -0400

    prepare to merge

--- scripts/cvo_geoemtric_test.bash
+++ scripts/cvo_geoemtric_test.bash
@@ -2,8 +2,8 @@
 export CUDA_VISIBLE_DEVICES=1
 cd build && make -j && cd .. && \
 for i in 04 
-#for i in 00 01 02 03 04 05 06 07 08 09 10
+#for i in 00 01 02 03 04 05
 do
     ./build/bin/cvo_align_gpu_lidar_raw /home/v9999/media/seagate_2t/kitti/lidar/dataset/sequences/$i cvo_params/cvo_geometric_params_gpu.txt \
-                                       cvo_geometric_$i.txt  0 2
+                                       cvo_geometric_$i.txt  0 100000
 done
--- scripts/cvo_semantic_test.bash
+++ scripts/cvo_semantic_test.bash
@@ -1,8 +1,9 @@
 
 cd build && make -j && cd .. && \
 #for i in 04
-export CUDA_VISIBLE_DEVICES=0
-for i in 04 01 00 02 03 05 06 07 08 09 10
+export CUDA_VISIBLE_DEVICES=1
+#for i in 04 01 00 02 03 05 
+for i in 06 07 08 09 10
 do
     ./build/bin/cvo_align_gpu_lidar_semantic_raw /home/v9999/media/seagate_2t/kitti/lidar/dataset/sequences/$i cvo_params/cvo_semantic_params_gpu.txt \
                                        cvo_semantic_$i.txt 0   10000
