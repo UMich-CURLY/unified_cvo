71c6f682096f82de9bbc9a01e6304289dd881a74
zeroAska <trotwoodray@gmail.com>
2020-08-19 11:43:54 -0400

    keep tuning

--- scripts/cvo_intensity_img_test.bash
+++ scripts/cvo_intensity_img_test.bash
@@ -1,7 +1,7 @@
 
 cd build && make -j && cd .. && \
-for i in  07 08 09 10   
-#for i in 00 01 02 03 04 05 06 07 08 09 10 
+#for i in  10   
+for i in 03 04 05 06 07 08 00 01 02 09 10 
 do
 	echo "new seq $i"
     ./build/bin/cvo_align_gpu_raw_img /home/rzh/media/sda1/ray/datasets/kitti/sequences/$i cvo_params/cvo_intensity_params_img.yaml \
