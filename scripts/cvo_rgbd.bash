6d0ee81a25d2deabaf783d0d0e6a4efa28a66cc0
zeroAska <trotwoodray@gmail.com>
2021-07-13 21:43:44 -0400

    hold, waiting to add multi-frame results

--- scripts/cvo_rgbd.bash
+++ scripts/cvo_rgbd.bash
@@ -4,7 +4,8 @@ cd build && make -j6 && cd .. && \
 # fr1
 # for i in freiburg1_desk freiburg1_desk2 freiburg1_room freiburg1_360 freiburg1_teddy freiburg1_xyz freiburg1_rpy freiburg1_plant
 
-for i in freiburg1_desk
+#for i in freiburg1_desk
+for i in freiburg3_structure_notexture_near
 #fr2
 # for i in
 
@@ -15,7 +16,7 @@ do
     echo "/********************** New Iteration *************************/"
 
     # gdb -ex run --args \
-	    ./build/bin/cvo_align_gpu_rgbd /home/justin/data/tum/$i cvo_params/cvo_rgbd_params.yaml \
-                                       cvo_rgbd_$i.txt 0 9999 
+	nvprof    ./build/bin/cvo_align_gpu_rgbd /home/rayzhang/media/Samsung_T5/tum/$i cvo_params/cvo_rgbd_params.yaml \
+                                       cvo_rgbd_$i.txt 0 2 
     
 done
