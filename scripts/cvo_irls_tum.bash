6178a0f9352f93dd25f0d14ba4c2da4238a82ceb
zeroAska <trotwoodray@gmail.com>
2021-12-23 16:06:32 -0500

    add geometric type

--- scripts/cvo_irls_tum.bash
+++ scripts/cvo_irls_tum.bash
@@ -3,11 +3,11 @@
 cd build 
 make -j 
 cd ..
-./build/bin/cvo_irls_tum /home/rayzhang/media/Samsung_T5/tum/freiburg1_desk2 cvo_params/cvo_intensity_params_irls_tum.yaml graph_defs/graph_tum2.txt /home/rayzhang/media/Samsung_T5/tum/freiburg1_desk2/CVO.txt 
+./build/bin/cvo_irls_tum /home/rayzhang/media/Samsung_T5/tum/freiburg1_desk2 cvo_params/cvo_intensity_params_irls_tum.yaml graph_defs/tum/1_graph.txt /home/rayzhang/media/Samsung_T5/tum/freiburg1_desk2/CVO.txt 
 echo "Evaluation: "
 echo "before BA ate:"
-python3 evaluate_ate_scale/evaluate_ate_scale.py  --verbose /home/rayzhang/media/Samsung_T5/tum/freiburg1_desk2/groundtruth.txt cvo_track_poses.txt 
+python3 evaluate_ate_scale/evaluate_ate_scale.py --plot before_traj.png --scale 1  --verbose /home/rayzhang/media/Samsung_T5/tum/freiburg1_desk2/groundtruth.txt cvo_track_poses.txt 
 echo "after BA ate:"
-python3 evaluate_ate_scale/evaluate_ate_scale.py  --verbose /home/rayzhang/media/Samsung_T5/tum/freiburg1_desk2/groundtruth.txt traj_out.txt
+python3 evaluate_ate_scale/evaluate_ate_scale.py --plot after_traj.png --scale 1 --verbose /home/rayzhang/media/Samsung_T5/tum/freiburg1_desk2/groundtruth.txt traj_out.txt
 
 
--- scripts/cvo_rgbd.bash
+++ scripts/cvo_rgbd.bash
@@ -4,8 +4,8 @@ cd build && make -j6 && cd .. && \
 # fr1
 # for i in freiburg1_desk freiburg1_desk2 freiburg1_room freiburg1_360 freiburg1_teddy freiburg1_xyz freiburg1_rpy freiburg1_plant
 
-#for i in freiburg1_desk
-for i in freiburg3_structure_notexture_near
+for i in freiburg1_360 freiburg3_structure_notexture_near freiburg1_desk2 freiburg1_room freiburg1_xyz freiburg1_rpy 
+#for i in freiburg3_structure_notexture_near
 #fr2
 # for i in
 
@@ -15,8 +15,9 @@ do
     echo ""
     echo "/********************** New Iteration *************************/"
 
-    # gdb -ex run --args \
-	nvprof    ./build/bin/cvo_align_gpu_rgbd /home/rayzhang/media/Samsung_T5/tum/$i cvo_params/cvo_rgbd_params.yaml \
-                                       cvo_rgbd_$i.txt 0 2 
+    #gdb -ex run --args \
+	#    ./build/bin/cvo_align_gpu_rgbd /home/rayzhang/media/Samsung_T5/tum/$i cvo_params/cvo_intensity_params_irls_tum.yaml \ #cvo_params/cvo_rgbd_params.yaml \
+	    ./build/bin/cvo_align_gpu_rgbd /home/rayzhang/media/Samsung_T5/tum/$i cvo_params/cvo_rgbd_params.yaml \
+                                       cvo_rgbd_$i.txt 0 100000 
     
 done
