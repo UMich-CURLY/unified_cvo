7b00971f22c429e5e9a4dd74cbc40f02cb42f329
zeroAska <trotwoodray@gmail.com>
2022-01-11 14:55:18 -0500

    add for merging

--- scripts/cvo_intensity_img_gpu0.bash
+++ scripts/cvo_intensity_img_gpu0.bash
@@ -1,15 +1,15 @@
 export CUDA_VISIBLE_DEVICES=0
 cd build && make -j && cd .. && \
-for i in  05
+#for i in  05
 #for i in  01 07 10  00 02 03 04 05 06 08 09 
-#for i in 01 10 07 02 05  04 06	
+for i in 04 01 03 10 07  05  02 06	
 #for i in  05  04 06	
 do
 	echo "new seq $i"
       #./build/bin/cvo_align_gpu_img /home/rayzhang/media/Samsung_T5/kitti/$i cvo_params/cvo_intensity_params_img_gpu0.yaml \
-	gdb -ex run --args \
+#	gdb -ex run --args \
       ./build/bin/cvo_align_gpu_img /home/rayzhang/media/Samsung_T5/kitti_stereo/dataset/sequences/kitti/$i cvo_params/cvo_intensity_params_img_gpu0.yaml \
-                                       cvo_intensity_img_gpu0_$i.txt 0 3
+                                       cvo_intensity_img_jan9_$i.txt 0 100000000
 
  	
 done
--- scripts/cvo_irls_tum.bash
+++ scripts/cvo_irls_tum.bash
@@ -3,7 +3,8 @@
 cd build 
 make -j 
 cd ..
-./build/bin/cvo_irls_tum /home/rayzhang/media/Samsung_T5/tum/freiburg1_desk2 cvo_params/cvo_intensity_params_irls_tum.yaml graph_defs/tum/1_graph.txt /home/rayzhang/media/Samsung_T5/tum/freiburg1_desk2/CVO.txt 
+gdb -ex run --args \
+./build/bin/cvo_irls_tum /home/rayzhang/media/Samsung_T5/tum/freiburg1_desk2 cvo_params/cvo_intensity_params_irls_tum.yaml graph_defs/tum/3_graph.txt /home/rayzhang/media/Samsung_T5/tum/freiburg1_desk2/CVO.txt 
 echo "Evaluation: "
 echo "before BA ate:"
 python3 evaluate_ate_scale/evaluate_ate_scale.py --plot before_traj.png --scale 1  --verbose /home/rayzhang/media/Samsung_T5/tum/freiburg1_desk2/groundtruth.txt cvo_track_poses.txt 
