0cfc64912fc6f76349fced07b71ff0638a0cc98c
zeroAska <trotwoodray@gmail.com>
2022-01-26 19:59:28 -0500

    add script for tartanair

---
+++ scripts/cvo_intensity_rgbd_tartan.bash
@@ -0,0 +1,17 @@
+export CUDA_VISIBLE_DEVICES=0
+cd build && make -j && cd .. && \
+#for i in  05
+#for i in  01 07 10  00 02 03 04 05 06 08 09 
+for i in carwelding office hospital 	
+do 
+	echo "new seq $i"
+      ./build/bin/cvo_align_gpu_rgbd_tartan /home/rayzhang/media/Samsung_T5/tartanair/$i/Easy/P001/ cvo_params/cvo_indoor_params.yaml \
+                                       cvo_tartan_indoor_$i.txt 0 30000
+done
+
+for i in abandonedfactory abandonedfactory_night amusement 	
+do
+	echo "new seq $i"
+      ./build/bin/cvo_align_gpu_rgbd_tartan /home/rayzhang/media/Samsung_T5/tartanair/$i/Easy/P001/ cvo_params/cvo_outdoor_params.yaml \
+                                       cvo_tartan_outdoor_$i.txt 0 30000
+done
