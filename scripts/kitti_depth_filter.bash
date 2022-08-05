58ab565d3ef0fae3bc850c82ca66f3074dddb8a6
zeroAska <trotwoodray@gmail.com>
2022-08-05 17:54:30 -0400

    prepare to merge

--- scripts/kitti_depth_filter.bash
+++ scripts/kitti_depth_filter.bash
@@ -1,13 +1,13 @@
 
 seq=05
-frame=23
+frame=117
 
-direll=1.0
-normalell=0.1
+direll=0.25
+normalell=0.04
 
 cd build
 make -j
 cd ..
 #valgrind --leak-check=yes \
-cuda-gdb -ex run --args \
-	./build/bin/kitti_depth_filtering  ../media/Samsung_T5/kitti_stereo/dataset/sequences/kitti/${seq}/ cvo_params/cvo_intensity_params_img_gpu0.yaml results/cvo_intensity_img_gpu0_oct25_best/${seq}.txt $frame 5 $direll $normalell
+#cuda-gdb -ex run --args \
+	./build/bin/kitti_depth_filtering  ../media/Samsung_T5/kitti/${seq}/ cvo_params/cvo_intensity_params_img_gpu0.yaml results/cvo_intensity_img_gpu0_oct25_best/${seq}.txt $frame 5 $direll $normalell
--- scripts/stack_pcd_test.bash
+++ scripts/stack_pcd_test.bash
@@ -2,10 +2,10 @@
 
 #for i in abandonedfactory endofworld gascola soulcity abandonedfactory_night ocean seasidetown #abandonedfactory abandonedfactory_night seasonsforest sea
 #for i in hospital #westerndesert  #hospital  #abandonedfactory_night #neighborhood  #seasidetown abandonedfactory  seasonsforest gascola hospital
-for i in 05 #westerndesert  #hospital  #abandonedfactory_night #neighborhood  #seasidetown abandonedfactory  seasonsforest gascola hospital
+for i in eth3d_sfm_lab_room_1 #westerndesert  #hospital  #abandonedfactory_night #neighborhood  #seasidetown abandonedfactory  seasonsforest gascola hospital
 do
     
-	./build/bin/stack_pcd_viewer /home/rayzhang/kitti_color_05 2700 0 0
-   # ./build/bin/stack_pcd_viewer /home/rayzhang/dsm/ 300 0 0
+	#./build/bin/stack_pcd_viewer /home/rayzhang/kitti_color_05 2700 0 0
+   ./build/bin/stack_pcd_viewer /home/rayzhang/dsm/$i 600 0 0
 done
 
