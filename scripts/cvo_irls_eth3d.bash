f6daac8c2b5b032b62c5bef33dc5fed1cfe3fdb9
zeroAska <trotwoodray@gmail.com>
2022-08-05 12:43:30 -0400

    random init for bunny example

---
+++ scripts/cvo_irls_eth3d.bash
@@ -0,0 +1,20 @@
+#/bin/bash
+seq=$1
+frame=$2
+
+
+# sfm_lab_room_1:   9   55
+
+cd build 
+make -j 
+cd ..
+#./build/bin/cvo_irls_tum /home/rayzhang/media/Samsung_T5/tum/freiburg3_structure_notexture_near cvo_params/cvo_intensity_params_irls_tum.yaml graph_defs/fr3/tum_fr3_structure_notexture_near/10_graph.txt /home/rayzhang/media/Samsung_T5/tum/freiburg3_structure_notexture_near/CVO.txt  
+#gdb -ex run --args \
+./build/bin/cvo_irls_tum /home/rayzhang/media/Samsung_T5/eth3d/${seq} cvo_params/cvo_intensity_params_irls_eth3d.yaml /home/rayzhang/dsm/eth3d_${seq}/${frame}_graph.small.txt 1 /home/rayzhang/media/Samsung_T5/eth3d/${seq}/ 
+#echo "Evaluation: "
+#echo "before BA ate:"
+#python3 evaluate_ate_scale/evaluate_ate_scale.py --plot before_traj.png --scale 1  --verbose /home/rayzhang/media/Samsung_T5/tum/freiburg1_${seq}/groundtruth.txt cvo_track_poses.txt 
+#echo "after BA ate:"
+#python3 evaluate_ate_scale/evaluate_ate_scale.py --plot after_traj.png --scale 1 --verbose /home/rayzhang/media/Samsung_T5/tum/freiburg1_${seq}/groundtruth.txt traj_out.txt
+
+
--- src/experiments/main_multi_frame_irls_bunny_random.cpp
+++ src/experiments/main_multi_frame_irls_bunny_random.cpp
@@ -1,7 +1,11 @@
+//#include <Eigen/src/Geometry/AngleAxis.h>
+#include <Eigen/Geometry>
 #include <iostream>
 #include <list>
 #include <cmath>
+//#include <pcl-1.9/pcl/impl/point_types.hpp>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/common/transforms.h>
 #include <vector>
 #include <utility>
 #include <random>
@@ -39,26 +43,50 @@ void write_transformed_pc(std::vector<cvo::CvoFrame::Ptr> & frames, std::string 
   pcl::io::savePCDFileASCII(fname, pc_all);
 }
 
-void gen_random_poses(std::vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eigen::Matrix4f>> & poses, int num_poses) {
+void gen_random_poses(std::vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eigen::Matrix4f>> & poses, int num_poses,
+                      float max_angle_axis=1.0 // max: [0, 1)
+                      ) {
   poses.resize(num_poses);
   std::random_device rd;
   std::mt19937 e2(rd());
-  std::uniform_real_distribution<> dist(0, 1);
+  std::uniform_real_distribution<> dist(0, max_angle_axis);
+  std::uniform_real_distribution<> dist_trans(0,1);
   for (int i = 0; i < num_poses; i++) {
 
     Eigen::Matrix3f rot;
+    Eigen::Vector3f axis;
+    axis << (float)dist_trans(e2), (float)dist_trans(e2), (float)dist_trans(e2);
+    axis = axis.normalized().eval();
+
     rot = Eigen::AngleAxisf(dist(e2)*M_PI, Eigen::Vector3f::UnitX())
-      * Eigen::AngleAxisf(dist(e2)*M_PI,  Eigen::Vector3f::UnitY())
+       * Eigen::AngleAxisf(dist(e2)*M_PI,  Eigen::Vector3f::UnitY())
       * Eigen::AngleAxisf(dist(e2)*M_PI, Eigen::Vector3f::UnitZ());
+
+    //rot = Eigen::AngleAxisf(dist(e2)*M_PI, axis );
 
     poses[i] = Eigen::Matrix4f::Identity();
     poses[i].block<3,3>(0,0) = rot;
-    poses[i](0,3) = dist(e2);
-    poses[i](1,3) = dist(e2);
-    poses[i](2,3) = dist(e2);
+    poses[i](0,3) = dist_trans(e2);
+    poses[i](1,3) = dist_trans(e2);
+    poses[i](2,3) = dist_trans(e2);
 
     std::cout<<"random pose "<<i<<" is \n"<<poses[i]<<"\n";
   }
+}
+template <typename T>
+void pcd_rescale(typename pcl::PointCloud<T>::Ptr pcd ){
+  Eigen::MatrixXf pcd_eigen(3, pcd->size());
+  for (int j = 0; j < pcd->size(); j++) {
+    pcd_eigen.col(j) = pcd->at(j).getVector3fMap();
+  }
+
+  float scale = (pcd_eigen.rowwise().maxCoeff() - pcd_eigen.rowwise().minCoeff()).norm();
+  std::cout << "scale = " << scale << std::endl;
+  pcd_eigen /= scale;
+
+  for (int j = 0; j < pcd->size(); j++)
+    pcd->at(j).getVector3fMap() = pcd_eigen.col(j);
+
 }
 
 int main(int argc, char** argv) {
@@ -67,36 +95,41 @@ int main(int argc, char** argv) {
   std::string in_pcd_fname(argv[1]);
   std::string cvo_param_file(argv[2]);
   int num_frames = std::stoi(argv[3]);
+  float max_angle_per_axis = std::stof(argv[4]);
   
   cvo::CvoGPU cvo_align(cvo_param_file);
 
   pcl::PointCloud<pcl::PointXYZ>::Ptr raw_pcd(new pcl::PointCloud<pcl::PointXYZ>);
-
-  pcl::io::loadPCDFile<pcl::PointXYZ> (in_pcd_fname, *raw_pcd);
+  pcl::PointCloud<pcl::PointNormal>::Ptr raw_pcd_normal(new pcl::PointCloud<pcl::PointNormal>);
+  pcl::io::loadPCDFile<pcl::PointNormal> (in_pcd_fname, *raw_pcd_normal);
+  copyPointCloud(*raw_pcd_normal, *raw_pcd);
+  pcd_rescale<pcl::PointXYZ>(raw_pcd);
   
-  // Create the filtering object
-  pcl::VoxelGrid<pcl::PointXYZ> sor;
-  sor.setInputCloud (raw_pcd);
-  sor.setLeafSize (0.025f, 0.025f, 0.025f);
-  sor.filter (*raw_pcd);
-  std::cout<<"after downsampling, num of points is "<<raw_pcd->size()<<std::endl;
 
-  std::shared_ptr<cvo::CvoPointCloud> raw (new cvo::CvoPointCloud(*raw_pcd));  
-
-  //std::vector<cvo::Mat34d_row, Eigen::aligned_allocator<cvo::Mat34d_row>> tracking_poses;
   std::vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eigen::Matrix4f>> tracking_poses;
-  gen_random_poses(tracking_poses, num_frames);
+  gen_random_poses(tracking_poses, num_frames, 0.2);
 
   // read point cloud
   std::vector<cvo::CvoFrame::Ptr> frames;
   std::vector<std::shared_ptr<cvo::CvoPointCloud>> pcs;
   for (int i = 0; i<num_frames; i++) {
-
-    std::shared_ptr<cvo::CvoPointCloud> pc(new cvo::CvoPointCloud(0,0));
+    // Create the filtering object
+    pcl::PointCloud<pcl::PointXYZ>::Ptr raw_pcd_transformed(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::transformPointCloud (*raw_pcd, *raw_pcd_transformed, tracking_poses[i]);
     
-    cvo::CvoPointCloud::transform(tracking_poses[i],
-                                  *raw,
-                                  *pc);
+    pcl::VoxelGrid<pcl::PointXYZ> sor;
+    sor.setInputCloud (raw_pcd_transformed);
+    sor.setLeafSize (0.025f, 0.025f, 0.025f);
+    sor.filter (*raw_pcd_transformed);
+    std::cout<<"after downsampling, num of points is "<<raw_pcd_transformed->size()<<std::endl;
+
+    std::shared_ptr<cvo::CvoPointCloud> pc (new cvo::CvoPointCloud(*raw_pcd_transformed));  
+    //std::shared_ptr<cvo::CvoPointCloud> pc(new cvo::CvoPointCloud(0,0));
+    
+    // cvo::CvoPointCloud::transform(tracking_poses[i],
+    //                               *raw,
+    //                               *pc);
+    
     cvo::Mat34d_row pose;
     Eigen::Matrix<double, 4,4, Eigen::RowMajor> pose44 = Eigen::Matrix<double, 4,4, Eigen::RowMajor>::Identity();
     pose = pose44.block<3,4>(0,0);
@@ -106,10 +139,16 @@ int main(int argc, char** argv) {
     frames.push_back(new_frame);
     pcs.push_back(pc);
   }
-  std::string f_name("before_BA.pcd");
+  
+  
+
+  //std::vector<cvo::Mat34d_row, Eigen::aligned_allocator<cvo::Mat34d_row>> tracking_poses;
+  std::cout<<"write to before_BA.pcd\n";
+  std::string f_name("before_BA_bunny.pcd");
   write_transformed_pc(frames, f_name);
 
   // std::list<std::pair<std::shared_ptr<cvo::CvoFrame::Ptr, cvo::CvoFrame::Ptr>> edges;
+  std::cout<<"Start constructing cvo edges\n";
   std::list<cvo::BinaryState::Ptr> edge_states;            
   for (int i = 0; i < num_frames; i++) {
     for (int j = i+1; j < num_frames; j++ ) {
@@ -131,12 +170,13 @@ int main(int argc, char** argv) {
 
   std::vector<bool> const_flags(frames.size(), false);
   const_flags[0] = true;
-  
+
+  std::cout<<"start align\n";
   cvo_align.align(frames, const_flags,
                   edge_states,  nullptr);
 
   //std::cout<<"Align ends. Total time is "<<time<<std::endl;
-  f_name="after_BA.pcd";
+  f_name="after_BA_bunny.pcd";
   write_transformed_pc(frames, f_name);
 
 
