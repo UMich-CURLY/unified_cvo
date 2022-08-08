e1a680919e4444696a237ea34d9ff4821ee4d4c2
zeroAska <trotwoodray@gmail.com>
2022-08-08 15:57:17 -0400

    add random init for bunny test

---
+++ include/UnifiedCvo/utils/def_assert.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#ifdef CVO_NDEBUG
+#undef NDEBUG
+#include <cassert>
+#endif
--- src/experiments/main_multi_frame_irls_bunny_random.cpp
+++ src/experiments/main_multi_frame_irls_bunny_random.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <list>
 #include <cmath>
+#include <fstream>
+#include <filesystem>
+#include "utils/def_assert.hpp"
 //#include <pcl-1.9/pcl/impl/point_types.hpp>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/common/transforms.h>
@@ -44,9 +47,11 @@ void write_transformed_pc(std::vector<cvo::CvoFrame::Ptr> & frames, std::string 
   pcl::io::savePCDFileASCII(fname, pc_all);
 }
 
-void add_normal_noise(pcl::PointCloud<pcl::PointNormal> & input,
-                      pcl::PointCloud<pcl::PointXYZ> & output,
-                      float sigma) {
+// pdf:  f(x) = \gamma * N(p, sigma) + (1- \gamma) * uniform()
+void add_gaussian_mixture_noise(pcl::PointCloud<pcl::PointNormal> & input,
+                                pcl::PointCloud<pcl::PointXYZ> & output,
+                                float ratio,
+                                float sigma) {
   
 }
 
@@ -59,23 +64,25 @@ void gen_random_poses(std::vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eige
   std::uniform_real_distribution<> dist(0, max_angle_axis);
   std::uniform_real_distribution<> dist_trans(0,1);
   for (int i = 0; i < num_poses; i++) {
-
-    Eigen::Matrix3f rot;
-    Eigen::Vector3f axis;
-    axis << (float)dist_trans(e2), (float)dist_trans(e2), (float)dist_trans(e2);
-    axis = axis.normalized().eval();
-
-    rot = Eigen::AngleAxisf(dist(e2)*M_PI, Eigen::Vector3f::UnitX())
-       * Eigen::AngleAxisf(dist(e2)*M_PI,  Eigen::Vector3f::UnitY())
-      * Eigen::AngleAxisf(dist(e2)*M_PI, Eigen::Vector3f::UnitZ());
-
+    if (i != 0) {
+      Eigen::Matrix3f rot;
+      Eigen::Vector3f axis;
+      axis << (float)dist_trans(e2), (float)dist_trans(e2), (float)dist_trans(e2);
+      axis = axis.normalized().eval();
+      
+      rot = Eigen::AngleAxisf(dist(e2)*M_PI, Eigen::Vector3f::UnitX())
+        * Eigen::AngleAxisf(dist(e2)*M_PI,  Eigen::Vector3f::UnitY())
+        * Eigen::AngleAxisf(dist(e2)*M_PI, Eigen::Vector3f::UnitZ());
+      poses[i] = Eigen::Matrix4f::Identity();
+      poses[i].block<3,3>(0,0) = rot;
+      poses[i](0,3) = dist_trans(e2);
+      poses[i](1,3) = dist_trans(e2);
+      poses[i](2,3) = dist_trans(e2);
+    } else {
+      poses[i] = Eigen::Matrix4f::Identity();
+    }
     //rot = Eigen::AngleAxisf(dist(e2)*M_PI, axis );
 
-    poses[i] = Eigen::Matrix4f::Identity();
-    poses[i].block<3,3>(0,0) = rot;
-    poses[i](0,3) = dist_trans(e2);
-    poses[i](1,3) = dist_trans(e2);
-    poses[i](2,3) = dist_trans(e2);
 
     std::cout<<"random pose "<<i<<" is \n"<<poses[i]<<"\n";
   }
@@ -93,7 +100,22 @@ void pcd_rescale(typename pcl::PointCloud<T>::Ptr pcd ){
 
   for (int j = 0; j < pcd->size(); j++)
     pcd->at(j).getVector3fMap() = pcd_eigen.col(j);
+}
 
+void eval_poses(std::vector<Sophus::SE3f> & estimates,
+                std::vector<Sophus::SE3f> & gt,
+                std::string & fname
+                ){
+  assert(std::filesystem::exists(fname));
+  std::ofstream err_f(fname,std::fstream::out |   std::ios::app);
+  float total_err = 0;
+  for (int i = 0; i < gt.size(); i ++) {
+    float err = (estimates[i].inverse() * gt[i]).log().norm();
+    total_err += err;
+  }
+  err_f << total_err<<"\n";
+  err_f.close();
+  //std::cout<<"Total: "<<counter<<" success out of "<<gt.size()<<"\n";
 }
 
 int main(int argc, char** argv) {
@@ -103,6 +125,7 @@ int main(int argc, char** argv) {
   std::string cvo_param_file(argv[2]);
   int num_frames = std::stoi(argv[3]);
   float max_angle_per_axis = std::stof(argv[4]);
+  int num_runs = std::stoi(argv[5]);
   
   cvo::CvoGPU cvo_align(cvo_param_file);
 
@@ -112,80 +135,109 @@ int main(int argc, char** argv) {
   copyPointCloud(*raw_pcd_normal, *raw_pcd);
   pcd_rescale<pcl::PointXYZ>(raw_pcd);
   
+  std::string fname("err_bunny.txt");
+  std::ofstream err_f(fname);
+  err_f.close();
+  for (int k = 0; k < num_runs; k++) {
+    std::vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eigen::Matrix4f>> tracking_poses;
+    gen_random_poses(tracking_poses, num_frames, 0.2);    
 
-  std::vector<Eigen::Matrix4f, Eigen::aligned_allocator<Eigen::Matrix4f>> tracking_poses;
-  gen_random_poses(tracking_poses, num_frames, 0.2);
+    std::vector<Sophus::SE3f> poses_gt;
+    std::transform(tracking_poses.begin(), tracking_poses.end(), std::back_inserter(poses_gt),
+                   [&](const Eigen::Matrix4f & in){
+                     Sophus::SE3f pose(in);
+                     return pose;
+                   });
 
-  // read point cloud
-  std::vector<cvo::CvoFrame::Ptr> frames;
-  std::vector<std::shared_ptr<cvo::CvoPointCloud>> pcs;
-  for (int i = 0; i<num_frames; i++) {
-    // Create the filtering object
-    pcl::PointCloud<pcl::PointXYZ>::Ptr raw_pcd_transformed(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::transformPointCloud (*raw_pcd, *raw_pcd_transformed, tracking_poses[i]);
+
+    // read point cloud
+    std::vector<cvo::CvoFrame::Ptr> frames;
+    std::vector<std::shared_ptr<cvo::CvoPointCloud>> pcs;
+    for (int i = 0; i<num_frames; i++) {
+      // Create the filtering object
+      pcl::PointCloud<pcl::PointXYZ>::Ptr raw_pcd_transformed(new pcl::PointCloud<pcl::PointXYZ>);
+      pcl::transformPointCloud (*raw_pcd, *raw_pcd_transformed, tracking_poses[i]);
     
-    pcl::VoxelGrid<pcl::PointXYZ> sor;
-    sor.setInputCloud (raw_pcd_transformed);
-    sor.setLeafSize (0.025f, 0.025f, 0.025f);
-    sor.filter (*raw_pcd_transformed);
-    std::cout<<"after downsampling, num of points is "<<raw_pcd_transformed->size()<<std::endl;
+      pcl::VoxelGrid<pcl::PointXYZ> sor;
+      sor.setInputCloud (raw_pcd_transformed);
+      sor.setLeafSize (0.025f, 0.025f, 0.025f);
+      sor.filter (*raw_pcd_transformed);
+      std::cout<<"after downsampling, num of points is "<<raw_pcd_transformed->size()<<std::endl;
 
-    std::shared_ptr<cvo::CvoPointCloud> pc (new cvo::CvoPointCloud(*raw_pcd_transformed));  
-    //std::shared_ptr<cvo::CvoPointCloud> pc(new cvo::CvoPointCloud(0,0));
+      std::shared_ptr<cvo::CvoPointCloud> pc (new cvo::CvoPointCloud(*raw_pcd_transformed));  
+      //std::shared_ptr<cvo::CvoPointCloud> pc(new cvo::CvoPointCloud(0,0));
     
-    // cvo::CvoPointCloud::transform(tracking_poses[i],
-    //                               *raw,
-    //                               *pc);
+      // cvo::CvoPointCloud::transform(tracking_poses[i],
+      //                               *raw,
+      //                               *pc);
     
-    cvo::Mat34d_row pose;
-    Eigen::Matrix<double, 4,4, Eigen::RowMajor> pose44 = Eigen::Matrix<double, 4,4, Eigen::RowMajor>::Identity();
-    pose = pose44.block<3,4>(0,0);
+      cvo::Mat34d_row pose;
+      Eigen::Matrix<double, 4,4, Eigen::RowMajor> pose44 = Eigen::Matrix<double, 4,4, Eigen::RowMajor>::Identity();
+      pose = pose44.block<3,4>(0,0);
     
 
-    cvo::CvoFrame::Ptr new_frame(new cvo::CvoFrameGPU(pc.get(), pose.data()));
-    frames.push_back(new_frame);
-    pcs.push_back(pc);
-  }
-  
-  
-
-  //std::vector<cvo::Mat34d_row, Eigen::aligned_allocator<cvo::Mat34d_row>> tracking_poses;
-  std::cout<<"write to before_BA.pcd\n";
-  std::string f_name("before_BA_bunny.pcd");
-  write_transformed_pc(frames, f_name);
-
-  // std::list<std::pair<std::shared_ptr<cvo::CvoFrame::Ptr, cvo::CvoFrame::Ptr>> edges;
-  std::cout<<"Start constructing cvo edges\n";
-  std::list<cvo::BinaryState::Ptr> edge_states;            
-  for (int i = 0; i < num_frames; i++) {
-    for (int j = i+1; j < num_frames; j++ ) {
-      //std::pair<cvo::CvoFrame::Ptr, cvo::CvoFrame::Ptr> p(frames[i], frames[j]);
-      //edges.push_back(p);
-      const cvo::CvoParams & params = cvo_align.get_params();
-      cvo::BinaryStateGPU::Ptr edge_state(new cvo::BinaryStateGPU(std::dynamic_pointer_cast<cvo::CvoFrameGPU>(frames[i]),
-                                                                  std::dynamic_pointer_cast<cvo::CvoFrameGPU>(frames[j]),
-                                                                  &params,
-                                                                  cvo_align.get_params_gpu(),
-                                                                  params.multiframe_num_neighbors,
-                                                                  params.multiframe_ell_init
-                                                             //dist / 3
-                                                             ));
-      edge_states.push_back((edge_state));
-      
+      cvo::CvoFrame::Ptr new_frame(new cvo::CvoFrameGPU(pc.get(), pose.data()));
+      frames.push_back(new_frame);
+      pcs.push_back(pc);
     }
+  
+  
+
+    //std::vector<cvo::Mat34d_row, Eigen::aligned_allocator<cvo::Mat34d_row>> tracking_poses;
+    std::cout<<"write to before_BA.pcd\n";
+    std::string f_name("before_BA_bunny.pcd");
+    write_transformed_pc(frames, f_name);
+
+    // std::list<std::pair<std::shared_ptr<cvo::CvoFrame::Ptr, cvo::CvoFrame::Ptr>> edges;
+    std::cout<<"Start constructing cvo edges\n";
+    std::list<cvo::BinaryState::Ptr> edge_states;            
+    for (int i = 0; i < num_frames; i++) {
+      for (int j = i+1; j < num_frames; j++ ) {
+        //std::pair<cvo::CvoFrame::Ptr, cvo::CvoFrame::Ptr> p(frames[i], frames[j]);
+        //edges.push_back(p);
+        const cvo::CvoParams & params = cvo_align.get_params();
+        cvo::BinaryStateGPU::Ptr edge_state(new cvo::BinaryStateGPU(std::dynamic_pointer_cast<cvo::CvoFrameGPU>(frames[i]),
+                                                                    std::dynamic_pointer_cast<cvo::CvoFrameGPU>(frames[j]),
+                                                                    &params,
+                                                                    cvo_align.get_params_gpu(),
+                                                                    params.multiframe_num_neighbors,
+                                                                    params.multiframe_ell_init
+                                                                    //dist / 3
+                                                                    ));
+        edge_states.push_back((edge_state));
+      
+      }
+    }
+
+    std::vector<bool> const_flags(frames.size(), false);
+    const_flags[0] = true;
+
+    std::cout<<"start align\n";
+    cvo_align.align(frames, const_flags,
+                    edge_states,  nullptr);
+
+    //std::cout<<"Align ends. Total time is "<<time<<std::endl;
+    f_name="after_BA_bunny_" + std::to_string(k)+".pcd";
+    write_transformed_pc(frames, f_name);
+    
+    std::vector<Sophus::SE3f> estimates;
+    std::transform(frames.begin(), frames.end(), std::back_inserter(estimates),
+                   [&](auto & frame){
+                     Eigen::Matrix<double, 3, 4, Eigen::RowMajor> pose_row = Eigen::Map<Eigen::Matrix<double, 3, 4, Eigen::RowMajor>>(frame->pose_vec);
+                     Eigen::Matrix<double , 3,4> pose_col = pose_row;
+                     Eigen::Matrix<float, 4, 4> pose_eigen = Eigen::Matrix4f::Identity();
+                     pose_eigen.block<3,4>(0,0) = pose_col.cast<float>();
+                     Sophus::SE3f pose(pose_eigen);
+                     return pose;
+                   });
+
+
+    eval_poses(estimates,
+               poses_gt,
+               fname
+               );
+
+
   }
-
-  std::vector<bool> const_flags(frames.size(), false);
-  const_flags[0] = true;
-
-  std::cout<<"start align\n";
-  cvo_align.align(frames, const_flags,
-                  edge_states,  nullptr);
-
-  //std::cout<<"Align ends. Total time is "<<time<<std::endl;
-  f_name="after_BA_bunny.pcd";
-  write_transformed_pc(frames, f_name);
-
-
   return 0;
 }
