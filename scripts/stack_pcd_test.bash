6713dd008dd49b8c76818d74d4d509aa00e213f3
zeroAska <trotwoodray@gmail.com>
2022-10-13 20:56:38 -0400

    cvo support for nn features

--- scripts/stack_pcd_test.bash
+++ scripts/stack_pcd_test.bash
@@ -2,7 +2,7 @@
 
 #for i in abandonedfactory endofworld gascola soulcity abandonedfactory_night ocean seasidetown #abandonedfactory abandonedfactory_night seasonsforest sea
 #for i in hospital #westerndesert  #hospital  #abandonedfactory_night #neighborhood  #seasidetown abandonedfactory  seasonsforest gascola hospital
-for i in eth3d_sfm_lab_room_1 #westerndesert  #hospital  #abandonedfactory_night #neighborhood  #seasidetown abandonedfactory  seasonsforest gascola hospital
+for i in eth3d_mannequin_3 #westerndesert  #hospital  #abandonedfactory_night #neighborhood  #seasidetown abandonedfactory  seasonsforest gascola hospital
 do
     
 	#./build/bin/stack_pcd_viewer /home/rayzhang/kitti_color_05 2700 0 0
