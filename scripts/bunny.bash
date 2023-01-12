eaa5b249ead68df2f90a366f5ab9c406609b1d22
zeroAska <trotwoodray@gmail.com>
2023-01-12 16:28:01 -0500

    add bunyy

--- scripts/bunny.bash
+++ scripts/bunny.bash
@@ -3,10 +3,10 @@ make -j
 cd ..
 for init_angle in 0.01 0.1 0.25
 do
-	for ratio in 0.001 0.01 0.1
+	for ratio in 0.01 0.1 0.25
 	do
 #gdb -ex run --args \
-    ./build/bin/cvo_irls_rand_pcd demo_data/bunny.pcd cvo_params/cvo_intensity_params_irls_bunny.yaml 4 ${init_angle} 2 toy_exp 1 $ratio 0.005 0.5
+    ./build/bin/cvo_irls_rand_pcd demo_data/bunny.pcd cvo_params/cvo_intensity_params_irls_bunny.yaml 4 ${init_angle} 50 toy_exp_${init_angle}_${ratio} 1 $ratio 0.005 0.5
 
 	done
 done
