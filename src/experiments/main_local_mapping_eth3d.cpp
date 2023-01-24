2a1b59102e4abeb9209219e4e436911e2b3619ea
zeroAska <trotwoodray@gmail.com>
2023-01-23 22:52:10 -0500

    add mapping test files

--- src/experiments/main_local_mapping_eth3d.cpp
+++ src/experiments/main_local_mapping_eth3d.cpp
@@ -19,10 +19,12 @@
 using namespace std;
 using namespace boost::filesystem;
 
+
 typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> MatrixXf_row;
 
 void map_to_pc(  semantic_bki::SemanticBKIOctoMap & map,
-                 cvo::CvoPointCloud & pc){
+                 cvo::CvoPointCloud & pc,
+                 int num_class){
 
   int num_pts = 0;
   for (auto it = map.begin_leaf(); it != map.end_leaf(); ++it) {
@@ -30,10 +32,11 @@ void map_to_pc(  semantic_bki::SemanticBKIOctoMap & map,
       num_pts++;
     }
   }
+  std::cout<<"Number of occupied points is "<<num_pts<<"\n";
     //num_classes_ = num_classes;
   std::vector<std::vector<float>> features;
   std::vector<std::vector<float>> labels;
-  pc.reserve(num_pts, 5, 0);
+  pc.reserve(num_pts, 5, num_class);
   int ind = 0;
   for (auto it = map.begin_leaf(); it != map.end_leaf(); ++it) {
     if (it.get_node().get_state() == semantic_bki::State::OCCUPIED) {
@@ -43,7 +46,7 @@ void map_to_pc(  semantic_bki::SemanticBKIOctoMap & map,
       xyz << p.x(), p.y(), p.z();
       //positions_.push_back(xyz);
       // features
-      std::vector<float> feature_vec(5, 0);
+      std::vector<float> feature_vec(5, num_class);
       it.get_node().get_features(feature_vec);
       Eigen::VectorXf feature = Eigen::Map<Eigen::VectorXf>(feature_vec.data(), 5);
       
@@ -52,22 +55,22 @@ void map_to_pc(  semantic_bki::SemanticBKIOctoMap & map,
       //std::vector<float> label(num_classes_, 0);
       //it.get_node().get_occupied_probs(label);
       //labels.push_back(label);
-      Eigen::VectorXf label, geometric_type;
+      Eigen::VectorXf label(num_class), geometric_type(2);
+      if (num_class)
+        label = Eigen::VectorXf::Ones(num_class);
+      geometric_type << 1.0, 0;
       pc.add_point(ind, xyz, feature,label, geometric_type );
+      ind++;      
     }
+
   }
+  std::cout<<"Export "<<ind<<" points from the bki map\n";
 }
 
 int main(int argc, char *argv[]) {
   // list all files in current directory.
   //You could put any file path in here, e.g. "/home/me/mwah" to list that directory
   std::cout<<argc<<std::endl;
-  int num_class= 1;
-  string n_class_str;
-  //if (argc > 5) {
-  //  n_class_str = argv[5];
-  //num_class = //stoi(n_class_str);
-  //}
 
   cvo::TumHandler tum(argv[1]);
   int total_iters = tum.get_total_number();
@@ -79,6 +82,7 @@ int main(int argc, char *argv[]) {
   int start_frame = stoi(argv[3]);
   int num_frames = stoi(argv[4]);
   std::string odom_file(argv[5]);
+  int num_class = std::stoi(argv[6]);
   
   vector<string> files;
   std::cout<<" cycle through the directory\n";
@@ -91,13 +95,13 @@ int main(int argc, char *argv[]) {
   // Set parameters
   int block_depth = 1;
   double sf2 = 1.0;
-  double ell = 1.0;
+  double ell = 0.075;
   float prior = 0.0f;
   float var_thresh = 1.0f;
-  double free_thresh = 0.65;
-  double occupied_thresh = 0.9;
-  double resolution = 0.05;
-  double free_resolution = 1;
+  double free_thresh = 0.7;
+  double occupied_thresh = 0.95;
+  double resolution = 0.02;
+  double free_resolution = 0.02;
   double ds_resolution = -1;
   double max_range = -1;
 
@@ -109,8 +113,8 @@ int main(int argc, char *argv[]) {
   
   // Build map
   //std::vector<cvo::CvoPointCloud> pc_vec(files.size());
-  semantic_bki::SemanticBKIOctoMap map_csm(resolution, block_depth, num_class + 1, sf2, ell, prior, var_thresh, free_thresh, occupied_thresh);
-  cvo::CvoPointCloud pc_full(5,0);
+  semantic_bki::SemanticBKIOctoMap map_csm(resolution, block_depth, num_class+1, sf2, ell, prior, var_thresh, free_thresh, occupied_thresh);
+  cvo::CvoPointCloud pc_full(5,num_class);
   int i = 0;
   for (; i+start_frame <= last_frame; i++) {
     
@@ -120,20 +124,28 @@ int main(int argc, char *argv[]) {
     tum.read_next_rgbd(source_rgb, source_dep);
     std::vector<uint16_t> source_dep_data(source_dep.begin<uint16_t>(), source_dep.end<uint16_t>());
 
-    //std::shared_ptr<cvo::Frame> source(new cvo::Frame(start_frame, source_rgb, source_dep,
-    //19, semantics_source, 
-    //                                                  calib, 1));
     std::shared_ptr<cvo::ImageRGBD<uint16_t>> source_raw(new cvo::ImageRGBD(source_rgb, source_dep_data));
     std::shared_ptr<cvo::CvoPointCloud> pc_original(new cvo::CvoPointCloud(*source_raw,
                                                                            calib
                                                                            //,cvo::CvoPointCloud::CANNY_EDGES
                                                                            ));
-    
+
+    cvo::CvoPointCloud pc_with_semantics(pc_original->num_features(), num_class);
+    pc_with_semantics.reserve(pc_original->size(), pc_original->num_features(), num_class);
+    for (int k = 0; k < pc_original->size(); k++) {
+      Eigen::VectorXf label(num_class);
+      if (num_class)
+        label = Eigen::VectorXf::Zero(num_class);
+      Eigen::VectorXf geometric_type(2);
+      geometric_type << 1, 0;
+      pc_with_semantics.add_point(k, pc_original->at(k), pc_original->feature_at(k),
+                                  label, geometric_type);
+    }
     
     // transform point cloud
     Eigen::Matrix4f transform = poses[i].cast<float>();
-    cvo::CvoPointCloud transformed_pc(5,0);
-    cvo::CvoPointCloud::transform(transform, *pc_original, transformed_pc);
+    cvo::CvoPointCloud transformed_pc(5,num_class);
+    cvo::CvoPointCloud::transform(transform, pc_with_semantics, transformed_pc);
     pc_full += transformed_pc;
 
     semantic_bki::point3f origin;
@@ -143,12 +155,15 @@ int main(int argc, char *argv[]) {
 
     // insert point cloud
     map_csm.insert_pointcloud_csm(&transformed_pc, origin, ds_resolution, free_resolution, max_range);
+    transformed_pc.write_to_color_pcd(std::to_string(i)+".pcd");
+    tum.next_frame_index();
   }
+  pc_full.write_to_pcd("pc_full.pcd");
   
   // Map to CVOPointCloud
   //cvo::CvoPointCloud cloud_out(&map_csm, num_class);
-  cvo::CvoPointCloud pc_map(5,0);
-  map_to_pc(map_csm, pc_map);
+  cvo::CvoPointCloud pc_map(5,num_class);
+  map_to_pc(map_csm, pc_map, num_class);
   //pc_vec[0].write_to_color_pcd(output_dir + "/" + "input_color.pcd");
   //pc_vec[0].write_to_label_pcd(output_dir + "/" + "input_semantics.pcd");
   pc_map.write_to_color_pcd("map.pcd");
