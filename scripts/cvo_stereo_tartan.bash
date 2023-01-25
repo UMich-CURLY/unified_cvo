dc955fa566b0033981102afb1f06a7e9d8f52bbb
zjaxxon <zjaxxon@gmail.com>
2023-01-25 03:50:00 -0500

    tartan script

--- scripts/cvo_stereo_tartan.bash
+++ scripts/cvo_stereo_tartan.bash
@@ -12,11 +12,11 @@ export CUDA_VISIBLE_DEVICES=0
 
 #done
 
-for i in oldtown
+for i in abandonedfactory_night amusement carwelding endofworld
 do
 	echo "new seq $i"
       ./build/bin/main_cvo_gpu_align_stereo_tartan /media/sdc1/rzh/tartanair/$i/Easy/P001 cvo_params/cvo_outdoor_params.yaml \
-                                       cvo_tartan_outdoor_$i.txt 0 30000
+                                       tartan_results/cvo_tartan_outdoor_$i.txt 0 30000
 #      python scripts/xyzq2kitti.py cvo_tartan_outdoor_$i.txt results/tartan/outdoor_easy_kitti_$i.txt
 	#python ~/.local/lib/python3.10/site-packages/evo/main_traj.py kitti --ref ground_truth/tartan/outdoor_easy_kitti_$i.txt  results/tartan/outdoor_easy_kitti_$i.txt   --plot_mode xyz --align --save_plot results/tartan/${i}_plot -p
 done
