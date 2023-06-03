12932f280e2eb91a50ee7626d2763084988d6f8c
zeroAska <trotwoodray@gmail.com>
2023-06-03 19:28:37 -0400

    add loop closure files

--- scripts/cvo_irls_kitti_loop.bash
+++ scripts/cvo_irls_kitti_loop.bash
@@ -2,21 +2,23 @@ cd build
 #cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j
 cd ..
-
+export CUDA_VISIBLE_DEVICES=1
 date=$1
 clear
 
     #skylabel=(196 112 -- 130  196 146 130)
-    seqs=( 05 )
+    #seqs=( 05 00 08 02 06 09 )
+    seqs=( 07   )
     for ind in ${!seqs[@]}
     do
         echo " Current Seq: ${i}"        
         i=${seqs[ind]}
         #sky=${skylabel[ind]}
 	folder=kitti_lidar_${i}_${date}
-        dataset_folder=/home/rayzhang/media/Samsung_T5/kitti_lidar/dataset/sequences/${i}/
-        #lc_file=/home/rayzhang/unified_cvo/demo_data/kitti_loop_closure/kitti_${i}.txt
-        lc_file=/home/rayzhang/unified_cvo/demo_data/kitti_loop_closure/kitti_${i}_loop_closure.g2o
+        #dataset_folder=/home/rayzhang/media/Samsung_T5/kitti_lidar/dataset/sequences/${i}/
+	dataset_folder=/home/rzh//media/sdg1/rzh/kitti_lidar/kitti_lidar/dataset/sequences/${i}/
+        lc_file=/home/rzh/unified_cvo/demo_data/kitti_loop_closure/kitti_${i}.txt
+        #lc_file=/home/rayzhang/unified_cvo/demo_data/kitti_loop_closure/kitti_${i}_loop_closure.g2o
 
 	rm -rf $folder
 	mkdir -p $folder
@@ -26,8 +28,9 @@ clear
         ### run global BA
         #gdb -ex run --args \
         #./build/bin/cvo_irls_lidar_loop $dataset_folder cvo_params/cvo_irls_kitti_ba_params.yaml 2 $folder/tracking_full.txt $lc_file  ba.txt 0 0 1000000 2.0 0 0 0 # > log_tartan_rgbd_${difficulty}_${i}.txt
-        gdb -ex run --args \
-        ./build/bin/cvo_irls_lidar_loop $dataset_folder cvo_params/cvo_irls_kitti_ba_params.yaml 2 $folder/tracking_full.txt $lc_file  ba.txt 0 0 1000000 0.5 0 0 1 # > log_tartan_rgbd_${difficulty}_${i}.txt
+        #gdb -ex run --args \
+        ./build/bin/cvo_irls_lidar_loop $dataset_folder cvo_params/cvo_irls_kitti_ba_params.yaml 2 $folder/tracking_full.txt $lc_file  ba.txt 0 0 1000000 0.5 0 1 0 # > log_tartan_rgbd_${difficulty}_${i}.txt
+        #./build/bin/cvo_irls_lidar_loop $dataset_folder cvo_params/cvo_irls_kitti_ba_params.yaml 3 $folder/tracking_full.txt $lc_file  ba.txt 0 0 1000000 4.0 0 0 # > log_tartan_rgbd_${difficulty}_${i}.txt
         mv *.pcd $folder/
         mv pgo.txt global.txt loop_closures.g2o tracking.txt ba.txt err_wrt_iters_*.txt groundtruth.txt $folder/
         cp ${dataset_folder}/poses.txt $folder/
